3b7b6aa2c45c1f2f099c6270de9ca75c2c380bf9
Laurel Perkins <lmperkins20@gmail.com>
2023-05-09 16:10:22 -0700

    added crud lambdas

---
+++ handleDelete/delete.test.js
@@ -0,0 +1,7 @@
+'use strict';
+
+describe('Testing delete method for People', () => {
+  xtest('Successfully deletes a Person by ID', () => {
+    
+  });
+});
---
+++ handleUpdate/update.tets.js
@@ -0,0 +1,7 @@
+'use strict';
+
+describe('Testing update method for People', () => {
+  xtest('Successfully update Person by ID', () => {
+    
+  });
+});
